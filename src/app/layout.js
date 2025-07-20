1cb2d19403aea2f45583e5214a8dacd95b06a97b
Aditya <adityarabbyoka3@gmail.com>
2025-07-20 20:54:02 +0700

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,22 +14,46 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "FesnukSave - Download Free Online Videos",
+  title: "FesnukSave - Free Facebook Video Downloader",
   description:
-    "Download videos from various platforms Facebook for free and quickly. FesnukSave is a safe and easy-to-use online tool.",
+    "Download Facebook videos for free and quickly. FesnukSave is a safe and easy-to-use online tool.",
   keywords: [
     "download video",
     "facebook downloader",
     "FesnukSave",
     "video downloader online",
+    "download Facebook videos",
+    "free video downloader",
+    "online video downloader",
+    "Facebook video download tool",
   ],
   authors: [{ name: "FesnukSave by Aditya Kurniawan" }],
   viewport: "width=device-width, initial-scale=1",
+  openGraph: {
+    title: "FesnukSave - Free Facebook Video Downloader",
+    description:
+      "Download Facebook videos for free and quickly with FesnukSave.",
+    url: "https://fesnuksave.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/public/fesnuksave.png",
+        width: 800,
+        height: 600,
+        alt: "FesnukSave Facebook Video Downloader",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://fesnuksave.vercel.app/" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
